15bd8b11e6ed21c25bf4c9d87ba915e661b2dbe7
jaydensar <jayden@jaydensar.net>
2022-04-25 22:54:23 -0400

    perf: remove unused deps and optimize release binary

--- server/src/main.rs
+++ server/src/main.rs
@@ -114,6 +114,8 @@ async fn main() {
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
+    println!("Listening on localhost:3000");
+
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
